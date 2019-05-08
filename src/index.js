2559dc5db1d6d4a1318b9c29b951875fcea09f85
rudovik <giguni@ukr.net>
2019-05-08 03:30:27 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT;
+
+// Define patsh for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.listen(port, () => {
+  console.log('Server is up on port ' + port + '.');
+});
